6f4a162c0e1484e14ee37130e6f111e62f7abbd9
Serverket <manuel.hernandez@riverovisualgroup.com>
2020-07-09 00:02:53 -0500

    User Counter for Home Page (needs translation).

---
+++ resources/assets/js/components/srcComponent/DashboardCounters.vue
@@ -0,0 +1,48 @@
+<template>
+<div>
+    <div class="col-lg-3 col-6">
+            <!-- small card -->
+            <div class="small-box bg-info panel-heading">
+              <div class="inner">
+                <h3>{{ amount }}</h3>
+
+                <p>Users</p>
+              </div>
+              <div class="icon">
+                <i class="fas fa-user-plus"></i>
+              </div>
+              <a href="/users" class="small-box-footer">
+                More info <i class="fas fa-arrow-circle-right"></i>
+              </a>
+            </div>
+          </div>
+</div>   
+</template>
+
+
+<script>
+    export default {
+        
+     mounted() {
+            console.log('Component DashboardCounters mounted.')
+           this.getData();
+           },
+     data: function () {
+          return {
+            amount: "",
+          }
+    },
+    props: {
+           prefUrl: { type: String,default:null},
+           router: "",
+          },
+    methods: {
+        getData: function (){
+         var url = this.prefUrl+this.router;
+            axios.get(url).then(response => {
+              this.amount=response.data.total;
+            });
+        },
+     }
+}
+</script>
