ebe8004fc57630a219606695b5439393ae6f4b8d
Serverket <manuel.hernandez@riverovisualgroup.com>
2020-09-10 10:09:50 -0500

    Styles finished.

--- resources/views/module_administration/reports/printProposal.blade.php
+++ resources/views/module_administration/reports/printProposal.blade.php
@@ -2,7 +2,7 @@
 @page {
     margin: 0cm 0cm;
     font-family: helvetica;
-    font-weight: normal; !important
+    font-weight: normal;
 }
 
     .page-break {
@@ -15,16 +15,18 @@
 
     body {
     margin: 1.5cm 1.5cm 2cm 1.5cm;
-    font-size: 13px;
-    background:white;
+    font-size: 12px;
+    background: white;
+    margin-top: 100px;
 }
 
     .container-header{
     position: fixed;
+    z-index: -1;
 }
 
     .header {
-	  background-color: #505050;
+	background-color: #505050;
     border-bottom: 4px solid #303030;
     width:100%;
   	height: 20px;
@@ -36,32 +38,32 @@
     float: left;
     margin-top: -75px;
   	margin-left: 10px;
-  	width: 120px;
-  	height: 120px;
+  	width: 90px;
+  	height: 90px;
     border-radius: 0px 0px 10px 10px;
   	z-index: 2;
 }
 
     .logo img {
-    width: 100px;
+    width: 70px;
     margin: 12px 5px 5px 10px;
 }
 
     .wm-one {
-    position:absolute;
+    position: absolute;
     top: 50px;
-  	left: 488px;
+  	left: 544px;
     z-index: 2;
-    opacity:0.5;
-    z-index:-1;
+    opacity: 0.5;
+    z-index: -1;
    }
 
    .wm-two {
-    position:absolute;
+    position: absolute;
     top: 480px;
     z-index: 2;
-    opacity:0.5;
-    z-index:-1;
+    opacity: 0.5;
+    z-index: -1;
    }
 
     .upper-right{
@@ -69,7 +71,7 @@
     width: 100px;
     height: 30px;
     margin-top: 50px;
-    margin-left: 700px;
+    margin-left: 695px;
     background-color: #ffc501;
     border-radius: 0px 0px 0px 10px;
     z-index: 2;
@@ -77,68 +79,67 @@
    /** Define the footer rules **/
     .footer {
     position: fixed; 
-    top:98%;
+    top: 98%;
     bottom: 0cm; 
     left: 0cm; 
     right: 0cm;
     height: 0cm;
-    width:100%;
-  	height:20px;
+    width: 100%;
+  	height: 20px;
     border-top: 4px solid #303030;
     background-color:#505050;
-  	z-index:1;
+  	z-index: 1;
+}
+
+    .footer img {
+    position: relative;
+    margin-top: 3px;
+    width: 12px;
 }
 
    .lower-block {
-    position:absolute;
+    position: absolute;
     background-color: #ffc501;
-    width:375px;
-    font-size:10.5px;
-    margin-top:-40px;
-    margin-left:95px;
-    margin-bottom:18px;
+    width: 430px;
+    font-size: 10.5px;
+    text-align: center;
+    margin-top: -40px;
+    margin-left: 75px;
+    margin-bottom: 18px;
     border-radius: 10px 10px 0px 0px;
     padding: 5px 5px 5px 5px;
-    z-index:2;
-    /*margin-left:205px;*/
-    /*height:40px;*/
-    /*margin-bottom:100px;*/
-    /*padding:3px;*/
+    z-index: 2;
 }
     .lower-left {
-    /*float:left;*/
-    /*margin-top:-10px;*/
-    display:inline-block; 
-    margin-top:-2px;
-    width:100px;
-    height:30px;
-    background-color:#ffc501;
+    display: inline-block; 
+    margin-top: -2px;
+    width: 100px;
+    height: 30px;
+    background-color: #ffc501;
     border-radius: 0px 10px 0px 0px;
-    z-index:2;
+    z-index: 2;
 }
 
     .lower-right {
-    /*float:right;*/
-    /*margin-left:700px;*/
-    display:inline-block; 
-    margin-top:-40px;
-    margin-left:694px;
-    width:100px;
-    height:30px;
-    background-color:#ffc501;
+    display: inline-block; 
+    margin-top: -38px;
+    margin-left: 696px;
+    width: 100px;
+    height: 30px;
+    background-color: #ffc501;
     border-radius: 10px 0px 0px 0px;
-    z-index:2;
+    z-index: 2;
 }
 
     table {
-    width:100%;
+    width: 100%;
      /*border:1px solid black;*/
 }
 
     .table-header {
-    background-color:#ffc501; 
-    font-size:17px; 
-    text-align:center;
+    background-color: #ffc501; 
+    font-size: 17px; 
+    text-align: center;
 }
 
     td,tr,th{
@@ -149,16 +150,34 @@
     font-weight: bold !important;
 }
 
+    .big {
+    font-size: 16px;
+}
+
+    .float-left {
+    float: left;
+    margin-left: 5px;
+}
+
+    .center {
+    text-align: center;
+    }
+
+    .prologue {
+    margin-top: 70px;
+}
+
     .just {
     text-align: justify;
 }
 
     .date {
-    float:right;
+    float: right;
+    margin-top: -40px;
 }
 
-    .client-info {
-    margin-top:70px;
+    .line-height {
+    line-height: 1.5;
 }
 
 </style>
@@ -170,18 +189,17 @@
 
   <div class="upper-right"></div>
    <div class="logo"><img src="img/logos/jd.png"></img></div>
+        <img class="wm-one" src="img/logos/1.png"></img>
+        <img class="wm-two" src="img/logos/2.png"></img>
 </div>
-
-<img class="wm-one" src="img/logos/1.png"></img>
-<img class="wm-two" src="img/logos/2.png"></img>
 
 <div class="footer">
 
 
      {{-- Page <span class="pagenum"></span> --}}
         <div class="lower-left"></div>
-        <div class="lower-block"><i class="fa fa-mail"></i>project@jdrivero.com <i class="fa fa-globe"></i>www.jdrivero.com <i class="fa fa-phone"></i>+ (214) 462-0623 <i class="fa fa-fax"></i>+1 (469) 466-9747 <br>
-        <i class="fa fa-map-marker"></i>9304 Forest Lane Suite #N274, Dallas, TX 75243.
+        <div class="lower-block"><img src="img/icon-email.png"></img> project@jdrivero.com <img src="img/icon-location.png"></img> www.jdrivero.com <img src="img/icon-phone.png"></img> +1 (214) 462-0623 / +1 (469) 466-9747 <br>
+        <img src="img/icon-point.png"></img> 9304 Forest Lane Suite #N274, Dallas, TX 75243.
         </div>
         <div class="lower-right"></div>
  </div>
@@ -190,33 +208,29 @@
 July 8th, 2020
 </div>       
 
-<div class="client-info bold">
-Mr. Gino Gallegos    <br>                                
+<div class="big bold">Mr. Gino Gallegos</div>                            
 gino2051@gmail.com   <br>
 C. (469) 258 0764    <br>
 Customer ID: XXXXX   <br>
-</div>
 
 <br>
 <div>
-RE: Professional Design Services Proposal <br>
+<div class="float-left">RE:</div><div class="float-left">Professional Design Services Proposal <br>
        12201 C. F. Hawn Freeway Dallas, TX 75253 <br>
        Proposal ID: XXXX <br>
        Precontract ID: XXX <br>
 </div>
 
-<br>
-<div >
-Dear Mr/Mrs. Gallegos
+<div class="prologue">
+Dear <b>Mr/Mrs. Gallegos</b>
 </div>
 
 <br>
 <div class="just">
-We are pleased to submit this proposal to provide Professional Design Services associated with the Commercial project at the reference address in Dallas, Texas. Based on our perception of the overall project objectives, we propose to perform the following scope of work.
+We are pleased to submit this proposal to provide Professional Design Services associated with the Commercial project at the reference address in Dallas, Texas. Based on our perception of the overall project objectives, we propose to perform the following <b>scope of work</b>.
 </div>
 
-
-        <ul>
+        <ul class="just">
 @foreach($proposal['0']->scope as $scope)
           <li>{!! nl2br($scope->description) !!}</li>
 @endforeach
@@ -226,18 +240,16 @@ We are pleased to submit this proposal to provide Professional Design Services a
 To comply with the scope of work we propose the following tasks:
 </div>
 <br>
-
-
  
- <table style="border-collapse: collapse;" cellspacing="0" cellpadding="1px" border="0">
+ <table style="border-collapse: collapse;" cellspacing="0" cellpadding="1px">
         <thead>
         <tr class="table-header bold">
           <th width="5%" align="center">#</th>
-          <th width="40%">DESCRIPTION</th>
+          <th width="55%">DESCRIPTION</th>
           <th width="10%" align="center">UNIT</th>
-          <th width="15%" >QTY</th>
-          <th width="15%" align="center">UP</th>
-          <th width="15%" align="center">AMOUNT</th>
+          <th width="10%" align="center">QTY</th>
+          <th width="10%" align="center">UP</th>
+          <th width="10%" align="center">AMOUNT</th>
         </tr>
         </thead>
 @php 
@@ -247,11 +259,9 @@ To comply with the scope of work we propose the following tasks:
 
 foreach ($proposalsDetails as $propDetail) {
 
-
-
                $acum = $acum + 1;
                 if ($acum % 2 == 0) {
-                    $background = "#d7d7d7";
+                    $background = "#ffffff";
                 } else {
                     $background = "#ffffff";
                 }
@@ -267,7 +277,7 @@ foreach ($proposalsDetails as $propDetail) {
                     $symbol = $moneySymbol;
                 }
 @endphp
-        <tr style="background-color:{{$background}}">
+        <tr class="line-height">
         <td width="5%" align="center">{{$acum2}}</td>
         <td width="40%" >{{$space}}{{$propDetail->serviceName}}</td>
         <td width="10%" align="center">{{$propDetail->unit}}</td>
@@ -291,8 +301,8 @@ foreach ($proposalsDetails as $propDetail) {
 </table>
 
 <br>
-<div style="text-align: center;">
-   <b>Payment breakdown:</b>
+<div class="big bold center">
+   <b>Payment Breakdown</b>
 </div>
 
 
@@ -305,7 +315,7 @@ foreach ($proposalsDetails as $propDetail) {
 <br>
  <table cellspacing="0" cellpadding="0" border="0"  >
        <tr>
-        <th class="table-header" colspan="1">Time Frame</th>
+        <th class="big bold">Time Frame</th>
        </tr>
        <tr> 
            <ul>
@@ -319,7 +329,7 @@ foreach ($proposalsDetails as $propDetail) {
 
  <table cellspacing="0" cellpadding="0" border="0"  >
        <tr>
-        <th class="table-header" colspan="1">Terms & Conditions</th>
+        <th class="big bold center">Terms & Conditions</th>
        </tr>
        <tr> 
            <ul>
