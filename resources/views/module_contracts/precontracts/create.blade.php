1fc9d63c3bf0e1d02a284fd3c8d1b1df89299229
Gustavo Lucena <gus-gyo-code@localhost.localdomain>
2021-01-13 16:04:15 -0400

    fixes for icons and add form proposals

--- resources/views/module_contracts/precontracts/create.blade.php
+++ resources/views/module_contracts/precontracts/create.blade.php
@@ -4,7 +4,7 @@
 <div class="create">
   <form  class="formulario" action="{{Route('precontracts.store')}}" method="POST">
     <div>
-    <h3><i class="fas fa-user-tie"></i> Nuevo Pre-contrato</h3>
+    <h3><i class="fas fa-file-alt"></i> Nuevo Pre-contrato</h3>
       <div class="boxes">
         @if ($errors->any())
           <div class="alert alert-danger">
@@ -18,56 +18,56 @@
         @endif
         {{csrf_field()}}
         <div class="inputother boxes2">
-          <label for="preId"><i class="far fa-id-card"></i> N° PRECONTRATO</label>
+          <label for="preId"><i class="fas fa-book"></i> N° PRECONTRATO</label>
           <input type="text" style="cursor: no-drop" class="input-label" id="preId" name="preId" value="{{ $preId}}" disabled="on">
         </div>
         <div class="inputother boxes2">
-                <label for="projectName"><i class="fas fa-user-friends"></i> NOMBRE DEL PROYECTO</label>
+                <label for="projectName"><i class="fas fa-file-signature"></i> NOMBRE DEL PROYECTO</label>
                 <input type="text" class="input-label" id="projectName" name="projectName" value="{{ old('projectName') }}" placeholder="NOMBRE DEL PROYECTO">
               </div>
         <div class="inputother boxes2">
-          <label for="precontractDate"><i class=""></i> FECHA</label>
+          <label for="precontractDate"><i class="fas fa-calendar-alt"></i> FECHA</label>
           <input class="input-label flatpickr" id="precontractDate" name="precontractDate" value="{{ old('precontractDate') }}">
         </div>
         <search-client pref-url='../'></search-client>
         <div class="inputother boxes2">
-          <label for="contractType"><i class=""></i> TIPO DE CONTRATO</label>
+          <label for="contractType"><i class="fas fa-file-contract"></i> TIPO DE CONTRATO</label>
           <select name="contractType" id="contractType">
               <option value="P" >P</option>
               <option value="S" >S</option>
           </select>
         </div>
         <div class="inputother boxes2">
-          <label for="propertyNumber"><i class=""></i> NUMERO DE LA PROPIEDAD</label>
+          <label for="propertyNumber"><i class="fas fa-home"></i> NUMERO DE LA PROPIEDAD</label>
           <input type="text" class="input-label" id="propertyNumber" name="propertyNumber" value="{{ old('propertyNumber') }}" placeholder="5924">
         </div>
         <div class="inputother boxes2">  
-          <label for="streetName"><i class=""></i> CALLE</label>
+          <label for="streetName"><i class="fas fa-road"></i> CALLE</label>
           <input type="text" class="input-label" id="streetName" name="streetName" maxlength="20" value="{{ old('streetName') }}" placeholder="AZALEA">
         </div>
         <div class="inputother boxes2">  
-          <label for="streetType"><i class=""></i> TIPO DE CALLE</label>
+          <label for="streetType"><i class="fas fa-street-view"></i> TIPO DE CALLE</label>
           <input type="text" class="input-label" id="streetType" name="streetType" maxlength="10" value="{{ old('streetType') }}" placeholder="LN">
         </div>
         <div class="inputother boxes2">  
-          <label for="suiteNumber"><i class=""></i> SUITE NUMBER</label>
+          <label for="suiteNumber"><i class="fas fa-building"></i> SUITE NUMBER</label>
           <input type="text" class="input-label" id="suiteNumber" name="suiteNumber" maxlength="20" value="{{ old('suiteNumber') }}" placeholder="SUITE 114 (Opcional)">
         </div>
         <div class="inputother boxes2">  
-          <label for="city"><i class=""></i> CIUDAD</label>
+          <label for="city"><i class="fas fa-map"></i> CIUDAD</label>
           <input type="text" class="input-label" id="city" name="city" maxlength="20" value="{{ old('city') }}" placeholder="DALLAS">
         </div>
         <div class="inputother boxes2">  
-          <label for="state"><i class=""></i> ESTADO</label>
+          <label for="state"><i class="fas fa-compass"></i> ESTADO</label>
           <input type="text" class="input-label" id="state" name="state" maxlength="20" value="{{ old('state') }}" placeholder="TX">
         </div>
         <div class="inputother boxes2">  
-          <label for="zipCode"><i class=""></i> CODIGO POSTAL</label>
+          <label for="zipCode"><i class="fas fa-envelope"></i> CODIGO POSTAL</label>
           <input type="number" class="input-label" id="zipCode" name="zipCode" value="{{ old('zipCode') }}" placeholder="75230">
         </div>
         <select-building-code pref-Url='../'></select-building-code>
         <div class="inputother boxes2">
-          <label for="constructionType"><i class=""></i> TIPO DE CONTRUCCION</label>
+          <label for="constructionType"><i class="fas fa-building"></i> TIPO DE CONTRUCCION</label>
             <select class="form-control" name="constructionType" id="constructionType">
               <option value="TYPE I" >TYPE I</option>
               <option value="TYPE II" >TYPE II</option>
@@ -77,11 +77,11 @@
             </select>
         </div>
         <div class="textarea">  
-          <label for="comment"><i class=""></i> {{__('initial_comment')}}</label>
+          <label for="comment"><i class="fas fa-comment-alt"></i> {{__('initial_comment')}}</label>
           <textarea name="comment" id="comment" rows="10">{{ old('comment') }}</textarea>
         </div>
         <div class="inputother boxes2">
-          <label for="currencyId"><i class=""></i> {{__('currency')}}</label>
+          <label for="currencyId"><i class="fas fa-donate"></i> {{__('currency')}}</label>
           <select name="currencyId" id="currencyId">
                 @foreach($currencies as $currency)
                       <option value="{{$currency->currencyId}}" > {{$currency->currencyName}} </option>
--- resources/views/module_contracts/precontracts/edit.blade.php
+++ resources/views/module_contracts/precontracts/edit.blade.php
@@ -4,7 +4,7 @@
 <div class="create">
   <form  class="formulario" action="{{Route('precontracts.update',['id' => $precontract[0]->precontractId])}}" method="POST">
     <div>
-    <h3><i class="fas fa-user-tie"></i> Editar Pre-contrato</h3>
+    <h3><i class="fas fa-file-alt"></i> Editar Pre-contrato</h3>
       <div class="boxes">
         @if ($errors->any())
           <div class="alert alert-danger">
@@ -22,20 +22,20 @@
           <input type="hidden" name="countryId" value="{{$precontract[0]->countryId}}">
           <input type="hidden" name="companyId" value="{{$precontract[0]->companyId}}">
         <div class="inputother boxes2">
-          <label for="precontractNumber"><i class="far fa-id-card"></i> N° PRECONTRATO</label>
+          <label for="precontractNumber"><i class="fas fa-book"></i> N° PRECONTRATO</label>
           <input type="text" style="cursor: no-drop" class="input-label" id="precontractNumber" name="precontractNumber" value="{{ $precontract[0]->preId  }}" disabled="on">
         </div>
         <div class="inputother boxes2">
-                <label for="projectName"><i class="fas fa-user-friends"></i> NOMBRE DEL PROYECTO</label>
+                <label for="projectName"><i class="fas fa-file-signature"></i> NOMBRE DEL PROYECTO</label>
                 <input type="text" class="input-label" id="projectName" name="projectName" value="{{ $precontract[0]->projectName  }}" placeholder="NOMBRE DEL PROYECTO">
               </div>
         <div class="inputother boxes2">
-          <label for="precontractDate"><i class=""></i> FECHA</label>
+          <label for="precontractDate"><i class="fas fa-calendar-alt"></i> FECHA</label>
           <input class="input-label flatpickr" id="precontractDate" name="precontractDate" value="{{ $precontract[0]->precontractDate  }}">
         </div>
         <search-client pref-url='../../' c-id="{{$precontract[0]->clientId}}" c-name="{{$precontract[0]->client->clientName}}" c-address="{{$precontract[0]->siteAddress}}"></search-client>
         <div class="inputother boxes2">
-          <label for="contractType"><i class=""></i> TIPO DE CONTRATO</label>
+          <label for="contractType"><i class="fas fa-file-contract"></i> TIPO DE CONTRATO</label>
           <select name="contractType" id="contractType">
                  @foreach($currencies as $currency)
                    @if ('P' == $precontract[0]->contractType)
@@ -49,31 +49,31 @@
             </select>
         </div>
         <div class="inputother boxes2">
-          <label for="propertyNumber"><i class=""></i> NUMERO DE LA PROPIEDAD</label>
+          <label for="propertyNumber"><i class="fas fa-home"></i> NUMERO DE LA PROPIEDAD</label>
           <input type="text" class="input-label" id="propertyNumber" name="propertyNumber" value="{{$precontract[0]->propertyNumber }}" placeholder="5924">
         </div>
         <div class="inputother boxes2">  
-          <label for="streetName"><i class=""></i> CALLE</label>
+          <label for="streetName"><i class="fas fa-road"></i> CALLE</label>
           <input type="text" class="input-label" id="streetName" name="streetName" maxlength="20" value="{{$precontract[0]->streetName }}" placeholder="AZALEA">
         </div>
         <div class="inputother boxes2">  
-          <label for="streetType"><i class=""></i> TIPO DE CALLE</label>
+          <label for="streetType"><i class="fas fa-street-view"></i> TIPO DE CALLE</label>
           <input type="text" class="input-label" id="streetType" name="streetType" maxlength="10" value="{{$precontract[0]->streetType }}" placeholder="LN">
         </div>
         <div class="inputother boxes2">  
-          <label for="suiteNumber"><i class=""></i> SUITE NUMBER</label>
+          <label for="suiteNumber"><i class="fas fa-building"></i> SUITE NUMBER</label>
           <input type="text" class="input-label" id="suiteNumber" name="suiteNumber" maxlength="20" value="{{$precontract[0]->suiteNumber }}" placeholder="SUITE 114 (Opcional)">
         </div>
         <div class="inputother boxes2">  
-          <label for="city"><i class=""></i> CIUDAD</label>
+          <label for="city"><i class="fas fa-map"></i> CIUDAD</label>
           <input type="text" class="input-label" id="city" name="city" maxlength="20" value="{{$precontract[0]->city }}" placeholder="DALLAS">
         </div>
         <div class="inputother boxes2">  
-          <label for="state"><i class=""></i> ESTADO</label>
+          <label for="state"><i class="fas fa-compass"></i> ESTADO</label>
           <input type="text" class="input-label" id="state" name="state" maxlength="20" value="{{$precontract[0]->state }}" placeholder="TX">
         </div>
         <div class="inputother boxes2">  
-          <label for="zipCode"><i class=""></i> CODIGO POSTAL</label>
+          <label for="zipCode"><i class="fas fa-envelope"></i> CODIGO POSTAL</label>
           <input type="number" class="input-label" id="zipCode" name="zipCode" value="{{$precontract[0]->zipCode }}" placeholder="75230">
         </div>
         <select-building-code 
@@ -82,7 +82,7 @@
              prop-building-code-group='{{$precontract[0]->groupId}}' 
              prop-project-use='{{$precontract[0]->projectUseId}}'></select-building-code>
         <div class="inputother boxes2">
-          <label for="constructionType"><i class="fas fa-tty"></i> TIPO DE CONTRUCCION</label>
+          <label for="constructionType"><i class="fas fa-building"></i> TIPO DE CONTRUCCION</label>
           <select name="constructionType" id="constructionType">
                  @if($precontract[0]->constructionType == 'TYPE I')
                       <option value="TYPE I" selected>TYPE I</option>
@@ -113,11 +113,11 @@
             </select>
         </div>
         <div class="textarea">  
-          <label for="comment"><i class=""></i> {{__('initial_comment')}}</label>
+          <label for="comment"><i class="fas fa-comment-alt"></i> {{__('initial_comment')}}</label>
           <textarea name="comment" id="comment" rows="10">"{{$precontract[0]->comment }}"</textarea>
         </div>
         <div class="inputother boxes2">
-          <label for="currencyId"><i class=""></i> {{__('currency')}}</label>
+          <label for="currencyId"><i class="fas fa-donate"></i> {{__('currency')}}</label>
           <select name="currencyId" id="currencyId">
               @foreach($currencies as $currency)
                    @if ($currency->currencyId == $precontract[0]->currencyId)
